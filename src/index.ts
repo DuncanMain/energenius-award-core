8a76e7a1f3eef0f7b55af0f05ce1ac51911446d0
rome6886 <duncanmain86@gmail.com>
2026-02-02 10:47:06 +0000

    Make index.ts safe placeholder (no side effects)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-import "dotenv/config";
-import { spend } from "./awardCore.js";
-
-async function run() {
-  await spend("new-user-1769684730549", "1", "should-fail");
-}
-
-run().catch((e) => {
-  console.error("ERROR:", e?.message);
-  process.exit(0);
-});
+console.log("EG Award Core: no HTTP server configured yet.");
 
 
 
